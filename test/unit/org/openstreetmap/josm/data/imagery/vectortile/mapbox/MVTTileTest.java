100e9ead750bec21f1981abaadd8f1fe5bbc0ac2
taylor.smock <taylor.smock@0c6e7542-c601-0410-84e7-c038aed88b3b>
2023-10-31 20:27:34 +0000

    Fix #23257: MVTTileTest has an infrequently hit race condition (patch by marcello, modified)
    The race condition is dependent upon calculation speed; if the layers get loaded
    prior to the image loading, it is ''possible'' for us to get to the point where
    we expect the image to be loaded, but it isn't yet.
    
    Modifications are as follows:
    * Reinitialize tileSource and loader before each test, but initialize the cache
      once and clear that between tests.
    
    git-svn-id: https://josm.openstreetmap.de/svn/trunk@18892 0c6e7542-c601-0410-84e7-c038aed88b3b

--- test/unit/org/openstreetmap/josm/data/imagery/vectortile/mapbox/MVTTileTest.java
+++ test/unit/org/openstreetmap/josm/data/imagery/vectortile/mapbox/MVTTileTest.java
@@ -8,14 +8,19 @@ import java.awt.image.BufferedImage;
 import java.util.Collections;
 import java.util.stream.Stream;
 
+import org.apache.commons.jcs3.access.behavior.ICacheAccess;
 import org.awaitility.Awaitility;
 import org.awaitility.Durations;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.openstreetmap.gui.jmapviewer.Tile;
 import org.openstreetmap.gui.jmapviewer.interfaces.TileJob;
 import org.openstreetmap.josm.TestUtils;
+import org.openstreetmap.josm.data.cache.BufferedImageCacheEntry;
 import org.openstreetmap.josm.data.cache.JCSCacheManager;
 import org.openstreetmap.josm.data.imagery.ImageryInfo;
 import org.openstreetmap.josm.data.imagery.TileJobOptions;
@@ -24,10 +29,29 @@ import org.openstreetmap.josm.data.imagery.TileJobOptions;
  * Test class for {@link MVTTile}
  */
 class MVTTileTest {
-    private MapboxVectorCachedTileLoader loader = new MapboxVectorCachedTileLoader(
-            null, JCSCacheManager.getCache("testMapillaryCache"), new TileJobOptions(1, 1, Collections.emptyMap(), 3600));
-    private MapboxVectorTileSource tileSource = new MapboxVectorTileSource(
-            new ImageryInfo("Test Mapillary", "file:/" + TestUtils.getTestDataRoot() + "pbf/mapillary/{z}/{x}/{y}.mvt"));
+    private static ICacheAccess<String, BufferedImageCacheEntry> cache;
+    private MapboxVectorTileSource tileSource;
+    private MapboxVectorCachedTileLoader loader;
+
+    @BeforeAll
+    static void classSetup() {
+        cache = JCSCacheManager.getCache("testMapillaryCache");
+    }
+
+    @AfterAll
+    static void classTearDown() {
+        cache.clear();
+        cache = null;
+    }
+
+    @BeforeEach
+    void setup() {
+        cache.clear();
+        tileSource = new MapboxVectorTileSource(new ImageryInfo("Test Mapillary", "file:/" + TestUtils.getTestDataRoot()
+          + "pbf/mapillary/{z}/{x}/{y}.mvt"));
+        final TileJobOptions options = new TileJobOptions(1, 1, Collections.emptyMap(), 3600);
+        loader = new MapboxVectorCachedTileLoader(null, cache, options);
+    }
 
     /**
      * Provide arguments for {@link #testMVTTile(BufferedImage, Boolean)}
@@ -50,7 +74,7 @@ class MVTTileTest {
         assertEquals(image, tile.getImage());
 
         TileJob job = loader.createTileLoaderJob(tile);
-        // we can't use the cache, because we request the same tile many times over
+        // Ensure that we are not getting a cached tile
         job.submit(true);
         Awaitility.await().atMost(Durations.ONE_SECOND).until(tile::isLoaded);
         if (isLoaded) {
